a908e66e2fd59e0fff48a991d41d30ceaff99dfc
guipeng1019 <guipeng1019@sina.com>
2016-07-03 23:33:52 +0800

    upload assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix - this function creates a "special matrix" that as input for the 2nd function.  
+## And it generates a list of 4 operations that could be called in 2nd function
+## which are: set matrix value, get matrix value, set matrix inversion value,& get matrix inversion value.
 
+
+makeCacheMatrix <- function(x = matrix()) {               ## Function to cache calculation results
+  Inv <- NULL 
+
+  set <- function(y) {                                    ## Operation: set matrix value
+    x <<- y
+    Inv <<- NULL
+  }
+  get <- function() x                                     ## Operation: get matrix value 
+  setInversion <- function(Inversion) Inv <<- Inversion   ## Operation: set matrix inversion 
+  getInversion <- function() Inv                          ## Operation: get matrix inversion
+  list(set = set, get = get,   
+       setInversion = setInversion,   
+       getInversion = getInversion)   
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - check the Inversion result from makeCacheMatrix. 
+## If it is in cache get it from cache, otherwise calculate it. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) {                          ## Calculate matrix inversion, x arg must be a makeCacheMatrix
+  Inv <- x$getInversion()                                 ## Call the operation inside x: get matrix inversion 
+  if(!is.null(Inv)) {                                     ## If the inversion is not NULL (already calculated)
+    message("getting cached data")
+    return(Inv)                                           ## Return the value from cache
+  }
+  data <- x$get()                                         ## If not, call the get matrix value operation inside 'x'
+  Inv <- solve(data, ...)                                 ## Calculate the inversion
+  x$setInversion(Inv)                                     ## and write it into cache so no repeated calculation next time
+  Inv
+  
+  }
